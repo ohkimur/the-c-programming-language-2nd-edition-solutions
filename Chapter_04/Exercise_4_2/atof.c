5ceb829717b8487083a4a7fb7882dff369eee37c
Daniel Costrasel <gleesik.dark@gmail.com>
2018-04-28 19:18:15 +0300

    Chapter 4

---
+++ Chapter_04/Exercise_4_2/atof.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAXLEN 500
+
+int getln(char line[], int lim);
+double atof(char s[]);
+
+int main()
+{
+    char line[MAXLEN];
+
+    getln(line, MAXLEN);
+    printf("%s", line);
+
+    printf("%f", atof(line));
+
+    return 0;
+}
+
+int getln(char line[], int lim)
+{
+    int i = 0, c;
+
+    while(i < lim - 1 && (c = getchar()) != '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+
+    if(c == '\n')
+    {
+        line[i++] = c;
+    }
+
+    line[i] = '\0';
+
+    return i;
+}
+
+double atof(char s[])
+{
+    double val, power;
+    int i, sign, exp_sign = 1, exp_pwr = 0;
+
+    for(i = 0; isspace(s[i]); ++i);
+
+    sign = (s[i] == '-') ? -1 : 1;
+
+    if(s[i] == '+' || s[i] == '-')
+    {
+        ++i;
+    }
+
+    for(val = 0.0; isdigit(s[i]); ++i)
+    {
+        val = 10.0 * val + (s[i] - '0');
+    }
+
+    if(s[i] == '.')
+    {
+        ++i;
+    }
+
+    for(power = 1.0; isdigit(s[i]); ++i)
+    {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10;
+    }
+
+    if(s[i] == 'e' || s[i] == 'E')
+    {
+        if(s[++i] == '-')
+        {
+            exp_sign = -1;
+            ++i;
+        }
+    }
+
+    while(isdigit(s[i]))
+    {
+        exp_pwr = 10 * exp_pwr + (s[i] - '0');
+        ++i;
+    }
+
+    while(exp_pwr)
+    {
+        if(exp_sign == -1)
+        {
+            power *= 10;
+        }
+        else
+        {
+            power /= 10;
+        }
+
+        --exp_pwr;
+    }
+
+    return sign * val / power;
+}
+
+// Exercise page: 87
+
+// OBS: Silence is golden.
