103d8ca2c05ae0a6bd89846f3eab9a2b3f2eca51
Daniel Costrasel <gleesik.dark@gmail.com>
2018-05-14 15:39:34 +0300

    Exercise_4_13

---
+++ Chapter_04/Exercise_4_13/reverse.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define MAXLEN 100
+
+void reverse(char str[]);
+
+int main(void)
+{
+    char str[MAXLEN] = "This is just a string";
+
+    printf("%s\n", str);
+    reverse(str);
+    printf("%s\n", str);
+
+    return 0;
+}
+
+void reverse(char str[])
+{
+    static int i = 0;
+    static int j = 0;
+
+    if(str[i] != '\0')
+    {
+        char c = str[i++];
+        reverse(str);
+
+        str[j++] = c;
+    }
+}
+
+// Exercise page: 102
+
+// OBS: Silence is golden.
