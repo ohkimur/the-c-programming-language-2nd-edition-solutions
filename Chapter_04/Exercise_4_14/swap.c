0c88832fe4ad2e93a5305b9b63d14d99191cd2bf
Daniel Costrasel <gleesik.dark@gmail.com>
2018-05-15 11:25:14 +0300

    Exercise_4_14

---
+++ Chapter_04/Exercise_4_14/swap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define swap(t, x, y) { t temp; temp = x; x = y; y = temp; };
+
+int main(void)
+{
+    int x = 2, y = 3;
+    int temp;
+
+    printf("x: %d, y: %d\n", x, y);
+
+    swap(int, x, y);
+    printf("x: %d, y: %d\n", x, y);
+
+    return 0;
+}
+
+// Exercise page: 105
+
+// OBS: A use of a block is very useful because there can be created local
+// variables that don't create conflicts with already existed entities.
