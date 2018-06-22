321bde3cb7f2e769a3148ddeeea362bfe733843e
Daniel Costrasel <gleesik>
2018-06-22 22:26:21 +0300

    Exercise_5_2

--- Chapter_05/Exercise_5_2/getfloat.c
+++ Chapter_05/Exercise_5_2/getfloat.c
@@ -1,8 +1,75 @@
 #include <stdio.h>
 
+#define MAXLEN 1000
+#define BUFFSIZE 100
+
+int getch(void);
+void ungetch(int c);
+int getfloat(int *pn);
+
 int main(void)
 {
+    
     return 0;
+}
+
+int bufp = 0;
+int buf[BUFFSIZE];
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if(bufp >= BUFFSIZE)
+    {
+        printf("ungetch: too many characters\n");
+    }
+    else
+    {
+        buf[bufp++] = c;
+    }
+}
+
+int getfloat(int *pn)
+{
+    int c, sign;
+
+    while(isspace(c = getch()));
+
+    if(!isdigit(c) && c != EOF && c != '+' && c != '-')
+    {
+        ungetch(c);
+        return 0;
+    }
+
+    sign = (c == '-') ? -1 : 1;
+
+    if(c == '+' || c == '-')
+    {
+        if(!isdigit(c = getch()))
+        {
+            ungetch(c);
+            ungetch(sign == 1 ? '+' : '-');
+            return 0;
+        }
+    }
+
+    for(*pn = 0; isdigit(c); c = getch())
+    {
+        *pn = 10 * *pn + (c - '0');
+    }
+
+    *pn = *pn * sign;
+
+    if(c != EOF)
+    {
+        ungetch(c);
+    }
+
+    return c;
 }
 
 // Exercise page: 111
