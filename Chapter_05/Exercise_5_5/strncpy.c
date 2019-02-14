d03f66822b2b8013e8e0f591932f2bab2ad7e605
Daniel Costrasel <gleesik.dark@gmail.com>
2019-02-14 13:32:49 +0200

    strcat, strcmp, strcpy that operate at most first n chars

---
+++ Chapter_05/Exercise_5_5/strncpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void strcpy_ptr(char *s, char *t, unsigned int n);
+
+int main(void)
+{
+    char s[100] = "This is the first string";
+    char *t = "Test is the second string, that is cool";
+    unsigned int nr_chars = 26;
+
+    strcpy_ptr(s, t, nr_chars);
+
+    puts(s);
+
+    return 0;
+}
+
+void strcpy_ptr(char *s, char *t, unsigned int n)
+{
+    while((*s++ = *t++) && --n);
+        ;
+}
+
+// Exercise page: 121
+
+// OBS: Silence is golden.
