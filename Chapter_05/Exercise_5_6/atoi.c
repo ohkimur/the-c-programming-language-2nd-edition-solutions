c212224ec3bbd35a3fcaee1bf360e78bb330d390
Daniel Costrasel <gleesik.dark@gmail.com>
2019-03-13 19:34:01 +0200

    atoi, itoa, reverse implemented

--- Chapter_05/Exercise_5_6/atoi.c
+++ Chapter_05/Exercise_5_6/atoi.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
+int atoi(const char *s);
+
 int main(void)
 {
-    
+    int i;
+    char *s = "12s3a";
+
+    i = atoi(s);
+
+    printf("atoi: %d", i);
 
     return 0;
+}
+
+int atoi(const char *s)
+{
+    int i = 0;
+
+    while(*s != '\0' && *s > '0' && *s < '9')
+    {
+        i = i * 10 + *s - '0';
+        ++s;
+    }
+
+    return i;
 }
 
 // Exercise page: 121
