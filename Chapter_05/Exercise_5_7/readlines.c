0271caa6c992345cbb2e73b01e55b3f7c98216b6
Daniel Costrasel <gleesik.dark@gmail.com>
2019-03-30 13:11:26 +0200

    Readlines implemented

--- Chapter_05/Exercise_5_7/readlines.c
+++ Chapter_05/Exercise_5_7/readlines.c
@@ -1,14 +1,133 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLINES 5000
+#define MAXLEN 1000
+
+char *lineptr[MAXLINES];
+char lines[MAXLINES][MAXLEN];
+
+void swap(char *array[], int i, int j);
+
+size_t get_line(char *s, const int strlen);
+
+int readlines(char *lineptr[], int maxlines);
+void writelines(char *lineptr[], int maxlines);
+void qsortlines(char *lineptr[], int left, int right);
+
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World Dude\n");
+    int nlines;
 
-    int a = 2;
+    char *lineptr[MAXLINES];
+    char line[MAXLEN];
 
-    a = 4;
-
-    printf("%d\n", a);
+    if(nlines = readlines(lineptr, 2))
+    {
+        qsortlines(lineptr, 0, nlines - 1);
+        writelines(lineptr, 2);
+        return 0;
+    }
+    else
+    {
+        puts("error: input too big to sort");
+        return 1;
+    }
+    
 
     return 0;
 }
+
+size_t get_line(char *s, const int strlen)
+{
+    size_t i = 0;
+
+    while((*s = getchar()) != EOF && *s != '\n' && ++i < strlen)
+    {
+        ++s;
+    }
+
+    *s = '\0';
+    
+    return i;
+}
+
+int readlines(char *lineptr[], int maxlines)
+{
+    int line_len;
+    int nlines;
+
+    char line_temp[MAXLEN];
+
+    nlines = 0;
+    while(nlines < maxlines)
+    {
+        if(line_len = get_line(line_temp, MAXLEN))
+        {
+            char *line = (char *)malloc(line_len * sizeof(char));
+
+            line_temp[line_len] = '\0'; // Replace \n with \0
+            strcpy(line, line_temp);
+            lineptr[nlines++] = line;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    return nlines;
+}
+
+void writelines(char *lineptr[], int maxlines)
+{
+    int i = 0;
+
+    while(i < maxlines)
+    {
+        puts(lineptr[i++]);
+    }
+}
+
+void swap(char *array[], int i, int j)
+{
+    char *temp;
+
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
+void qsortlines(char *lineptr[], int left, int right)
+{
+    int i;
+    int last;
+
+    if(left >= right)
+    {
+        return;
+    }
+
+    swap(lineptr, left, (left + right) / 2);
+
+    last = left;
+
+    for(i = left + 1; i <= right; ++i)
+    {
+        if(strcmp(lineptr[i], lineptr[left]) < 0)
+        {
+            swap(lineptr, ++last, i);
+        }
+    }
+
+    swap(lineptr, left, last);
+
+    qsortlines(lineptr, left, last - 1);
+    qsortlines(lineptr, left + 1, last);    
+}
+
+// Exercise page: 124
+
+// OBS: Silence is golden.
