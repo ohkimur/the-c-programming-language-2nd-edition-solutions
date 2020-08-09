1a12e127ba89d959620f06c2b6531afdf1eaf0e8
Daniel Costrasel <gleesik.dark@gmail.com>
2020-08-09 22:54:10 +0300

    Renamed all files to lowercase.

---
+++ chapter_03/exercise_3_1/binsearch.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <time.h>
+
+#define MAXLEN 10000
+#define ITERATIONS 100000
+
+void initVector(int v[], int n);
+void printVector(int v[], int n);
+
+int binsearch_kr(int x, int v[], int n);
+int binsearch(int x, int v[], int n);
+
+void test_binsearch(int binsearch(int x, int v[], int n), int x, int v[], int n);
+
+int main()
+{
+  int v[MAXLEN];
+
+  initVector(v, MAXLEN);
+  // printVector(v, MAXLEN);
+
+  int x = -1;
+
+  // Test binsearch_kr()
+  test_binsearch(binsearch_kr, x, v, MAXLEN);
+
+  // Test binsearch()
+  test_binsearch(binsearch, x, v, MAXLEN);
+
+  return 0;
+}
+
+void initVector(int v[], int n)
+{
+  int i;
+  for(i = 0; i < n; ++i)
+  {
+    v[i] = i;
+  }
+}
+
+void printVector(int v[], int n)
+{
+  int i;
+  
+  printf("[ ");
+  for(i = 0; i < n; ++i)
+  {
+    (i != n - 1) ? printf("%d, ", v[i]) : printf("%d ]\n", v[i]);
+  }
+}
+
+int binsearch_kr(int x, int v[], int n)
+{
+  int low, mid, high;
+
+  low = 0;
+  high = n - 1;
+  while(low <= high)
+  {
+    mid = (low + high)/2;
+
+    if(x < v[mid])
+    {
+      high = mid - 1;
+    }
+    else if(x > v[mid])
+    {
+      low = mid + 1;
+    }
+    else
+    {
+      return mid;
+    }
+  }
+
+  return -1;
+}
+
+int binsearch(int x, int v[], int n)
+{
+  int low, mid, high;
+
+  low = 0;
+  high = n - 1;
+  while(low <= high)
+  {
+    mid = (low + high)/2;
+
+    if(x < v[mid])
+    {
+      high = mid - 1;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+
+  if(x == v[low - 1])
+  {
+    return low - 1;
+  }
+
+  return -1;
+}
+
+void test_binsearch(int binsearch(int x, int v[], int n), int x, int v[], int n)
+{
+  static int test_nr = 0;
+  long clocks = clock();
+
+  int i;
+  for(i = 0; i < ITERATIONS; ++i)
+  {
+    binsearch(x, v, n);
+  }
+
+  clocks = clock() - clocks;
+
+  printf("test_%d: %lu clocks (%.4f seconds)\n", test_nr, clocks, (double)clocks/CLOCKS_PER_SEC);
+
+  ++test_nr;
+}
+
+// Exercise page: 72
+
+// OBS: By using a simple test in the for loop there is a chance to increase the
+// binsearch() execution speed. Just modifying an if-else statement from 3 branches 
+// with two condition testing to 2 branches with 1 condition testing will not
+// increase drasticaly the program performance on modern machines, but is a better
+// aproach.
---
+++ chapter_03/exercise_3_6/itoa.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define MAXLEN 1000
+
+int ilen(int a);
+void intToArray(int n, char s[], int padding);
+
+int main()
+{
+  int n = 1995;
+  char number_str[MAXLEN];
+
+  intToArray(n, number_str, 6);
+  printf("%s\n", number_str);
+
+  return 0;
+}
+
+int ilen(int a)
+{
+  int i = 0;
+
+  do
+  {
+    ++i;
+  }
+  while(a /= 10);
+
+  return i;
+}
+
+void intToArray(int n, char s[], int padding)
+{
+  int i = 0, sign = n;
+
+  do
+  {
+    s[i++] = abs(n % 10) + '0';
+  }
+  while(n /= 10);
+
+  if(sign < 0)
+  {
+    s[i++] = '-';
+  }
+
+  int len = ilen(sign);
+  while(len < padding)
+  {
+    s[i++] = ' ';
+    --padding;
+  }
+
+  s[i] = '\0';
+
+  strrev(s);
+}
+
+// Exercise page: 78
+
+// OBS: Silence is golden.
