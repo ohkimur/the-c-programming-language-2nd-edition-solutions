05e86ccecaec039324d34d69d684f5e777ed1497
ILikeToHelp <56758489+ILikeToHelp@users.noreply.github.com>
2020-08-10 16:42:35 +0100

    Update copy_io_nl.c

--- chapter_01/exercise_1_12/copy_io_nl.c
+++ chapter_01/exercise_1_12/copy_io_nl.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
-
 main()
 {
-  int state = OUT;
-
   char c;
   while((c = getchar()) != EOF)
   {
     if(c != ' ' && c != '\t' && c != '\n')
     {
-      state = IN;
       putchar(c);
     }
-    else if (state == IN)
+    else
     {
-      state = OUT;
       putchar('\n');
     }
   }
 }
-
-// Exercise page: 35
-
-// OBS: In this program I recycled the idea of IN/OUT for a word.
