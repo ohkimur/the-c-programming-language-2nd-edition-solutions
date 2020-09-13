e11706d3cedaca3a5d0a42be4003450af489b2b8
kotoffski <mr.kotoffski@gmail.com>
2020-09-13 16:41:35 +0300

    Updated solution for exercise 1.12

--- chapter_01/exercise_1_12/copy_io_nl.c
+++ chapter_01/exercise_1_12/copy_io_nl.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
+#define OUT 0
+#define IN 1
+
 int main(void)
 {
   char c;
+  int state;
+
+  state = OUT;
   while ((c = getchar()) != EOF)
   {
     if (c != ' ' && c != '\t' && c != '\n')
     {
       putchar(c);
+      state = IN;
     }
-    else
+    else if (state)
     {
       putchar('\n');
+      state = OUT;
     }
   }
-  
+
   return 0;
 }
-
