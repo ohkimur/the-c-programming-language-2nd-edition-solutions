d89a68566a9a2555fab7228504859b37f408ee13
Daniel Costrasel <gleesik.dark@gmail.com>
2021-02-18 17:28:11 +0200

    fix: improved the detab solution

--- chapter_01/exercise_1_20/detab.c
+++ chapter_01/exercise_1_20/detab.c
@@ -1,73 +1,40 @@
 #include <stdio.h>
 
-#define MAXLINE 1000
-#define TABWIDTH 8
-
-int getln(char line[], int limit);
-void detab(char line[], char tline[]);
+#define TAB_WIDTH 8
 
 int main(void)
 {
-  char line[MAXLINE];
-  char tline[MAXLINE];
+  int c;
+  int col = 0;
+  char spaces;
 
-  getln(line, MAXLINE);
-  detab(line, tline);
-  printf("%s", tline);
-
-  return 0;
-}
-
-int getln(char line[], int limit)
-{
-  int c, i;
-
-  i = 0;
-  while (i < limit - 1 && (c = getchar()) != EOF && c != '\n')
+  while ((c = getchar()) != EOF)
   {
-    line[i++] = c;
-  }
-
-  if (c == '\n')
-  {
-    line[i++] = '\n';
-  }
-
-  line[i] = '\0';
-
-  return i;
-}
-
-void detab(char line[], char tline[])
-{
-  int i = 0, j = 0;
-  int spacew = 0;
-
-  while (line[i] != '\0')
-  {
-    spacew = TABWIDTH - j % TABWIDTH;
-
-    if (line[i] == '\t')
+    if (c == '\t')
     {
-      printf("spacew: %d\n", spacew);
+      spaces = TAB_WIDTH - col % TAB_WIDTH;
+      col += spaces;
 
-      while (spacew-- > 0)
+      while (spaces)
       {
-        tline[j++] = '-';
+        putchar('#');
+        --spaces;
       }
-
-      ++i;
     }
     else
     {
-      tline[j++] = line[i++];
+      putchar(c);
+
+      if (c == '\n')
+      {
+        col = 0;
+      }
+      else
+      {
+        ++col;
+      }
     }
   }
 
-  if (line[i] == '\n')
-  {
-    tline[j++] = line[i];
-  }
-
-  tline[j] = '\0';
+  return 0;
 }
