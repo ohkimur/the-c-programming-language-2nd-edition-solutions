eb217118137fac7c6b50e6394993b943ef7f660c
Daniel Costrasel <gleesik.dark@gmail.com>
2021-02-18 23:46:12 +0200

    fix: tab stops when no arg provided

--- chapter_05/exercise_5_11/detab.c
+++ chapter_05/exercise_5_11/detab.c
@@ -11,20 +11,24 @@ int main(int argc, char *argv[])
   char nr_of_spaces;
   char tab_width = DEFAULT_TAB_WIDTH;
 
-  int arg_pos = 1;
-  int nr_of_tabs = argc - 1;
+  int arg_pos = 0;
+  int nr_of_tab_stops = argc - 1;
 
   while ((c = getchar()) != EOF)
   {
     if (c == '\t')
     {
-      if (nr_of_tabs)
+      if (nr_of_tab_stops)
       {
-        tab_width = atoi(argv[arg_pos++]);
-        --nr_of_tabs;
+        tab_width = atoi(argv[++arg_pos]);
+        --nr_of_tab_stops;
+      }
+      else if (argc > 1)
+      {
+        tab_width = 0;
       }
 
-      nr_of_spaces = tab_width - line_pos % tab_width;
+      nr_of_spaces = tab_width ? tab_width - line_pos % tab_width : 0;
 
       while (nr_of_spaces)
       {
@@ -39,8 +43,8 @@ int main(int argc, char *argv[])
       if (c == '\n')
       {
         line_pos = 0;
-        arg_pos = 1;
-        nr_of_tabs = argc - 1;
+        arg_pos = 0;
+        nr_of_tab_stops = argc - 1;
       }
       else
       {
