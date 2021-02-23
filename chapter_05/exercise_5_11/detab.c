fbbf2337e1cb0279bce022e462d1c02e2665d1ca
Daniel Costrasel <gleesik.dark@gmail.com>
2021-02-23 22:05:53 +0200

    fix: improved detab to work as expand

--- chapter_05/exercise_5_11/detab.c
+++ chapter_05/exercise_5_11/detab.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
-#define TAB_LENGTH 8
+#define DEFAULT_TAB_LENGTH 8
 
 int is_str_uint(char *str);
 int is_tab_stop_arg_list_valid(int argc, char *argv[]);
@@ -17,24 +17,46 @@ int main(int argc, char *argv[])
   }
 
   int c;
-  int tab_length = TAB_LENGTH;
-  char nr_of_spaces;
+  int tab_stop = DEFAULT_TAB_LENGTH;
+  int arg_pos = 1;
+  int line_pos = 0;
+  int nr_of_spaces;
+  int nr_of_tab_stops = argc - 1;
 
   while ((c = getchar()) != EOF)
   {
     if (c == '\t')
     {
-      nr_of_spaces = tab_length;
+      if (nr_of_tab_stops)
+      {
+        tab_stop = atoi(argv[arg_pos++]);
+        --nr_of_tab_stops;
+      }
+      else if (argc > 1)
+      {
+        tab_stop = 1;
+      }
+
+      nr_of_spaces = tab_stop - line_pos % tab_stop;
 
       while (nr_of_spaces)
       {
         putchar(' ');
+        ++line_pos;
         --nr_of_spaces;
       }
     }
     else
     {
       putchar(c);
+      ++line_pos;
+
+      if (c == '\n')
+      {
+        arg_pos = 1;
+        line_pos = 0;
+        nr_of_tab_stops = argc - 1;
+      }
     }
   }
 
