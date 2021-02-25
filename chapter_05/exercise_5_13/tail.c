d2e6be8977778160a14615af3736eb3cd9e3acc3
Daniel Costrasel <gleesik.dark@gmail.com>
2021-02-25 18:32:38 +0200

    chore: created the base for tail program

---
+++ chapter_05/exercise_5_13/tail.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define DEFAULT_N_LINES 10
+
+int is_str_uint(char *str);
+int is_arg_list_valid(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+  if (!is_arg_list_valid(argc, argv))
+  {
+    puts("ERROR: Invalid arguments.\n");
+    return EXIT_FAILURE;
+  }
+
+  unsigned int n_lines = DEFAULT_N_LINES;
+
+  return EXIT_SUCCESS;
+}
+
+int is_str_uint(char *str)
+{
+  for (unsigned int i = 0; i < strlen(str); ++i)
+  {
+    if (!isdigit(str[i]))
+    {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int is_arg_list_valid(int argc, char *argv[])
+{
+  if (argc > 2 || (argc == 2 && (argv[argc - 1][0] != '-' || !is_str_uint(argv[argc - 1] + 1))))
+  {
+    return 0;
+  }
+  return 1;
+}
