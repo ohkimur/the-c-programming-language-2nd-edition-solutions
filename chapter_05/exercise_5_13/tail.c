c4a60791b8be589b8cd0a8a4bfca0051cb932202
Daniel Costrasel <gleesik.dark@gmail.com>
2021-02-25 20:13:55 +0200

    solution: implemented partial solution for the tail program

--- chapter_05/exercise_5_13/tail.c
+++ chapter_05/exercise_5_13/tail.c
@@ -4,9 +4,13 @@
 #include <ctype.h>
 
 #define DEFAULT_N_LINES 10
+#define MAX_LINE_LENGTH 1000
 
 int is_str_uint(char *str);
 int is_arg_list_valid(int argc, char *argv[]);
+
+size_t read_lines(char *line_ptr[], size_t max_nr_of_lines);
+void write_lines(char *line_ptr[], const size_t nr_of_lines);
 
 int main(int argc, char *argv[])
 {
@@ -16,14 +20,25 @@ int main(int argc, char *argv[])
     return EXIT_FAILURE;
   }
 
-  unsigned int n_lines = DEFAULT_N_LINES;
+  unsigned int nr_of_lines_to_print = DEFAULT_N_LINES;
 
   if (argc == 2)
   {
-    n_lines = atoi(argv[argc - 1] + 1);
+    nr_of_lines_to_print = atoi(argv[argc - 1] + 1);
   }
 
-  printf("%d", n_lines);
+  int nr_of_lines;
+  char *line_ptr[MAX_LINE_LENGTH];
+
+  if ((nr_of_lines = read_lines(line_ptr, MAX_LINE_LENGTH)) != -1)
+  {
+    write_lines(line_ptr, nr_of_lines);
+  }
+  else
+  {
+    puts("ERROR: Input too large.\n");
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
@@ -47,4 +62,43 @@ int is_arg_list_valid(int argc, char *argv[])
     return 0;
   }
   return 1;
+}
+
+size_t read_lines(char *line_ptr[], size_t max_nr_of_lines)
+{
+  size_t line_length;
+  size_t nr_of_lines = 0;
+  size_t bufsize = MAX_LINE_LENGTH;
+
+  char *p = NULL;
+  char *line = NULL;
+
+  if ((line = (char *)malloc(bufsize * sizeof(char))) == NULL)
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  while ((line_length = getline(&line, &bufsize, stdin)) != -1)
+  {
+    if (nr_of_lines >= max_nr_of_lines || (p = (char *)malloc(line_length * sizeof(char))) == NULL)
+    {
+      return -1;
+    }
+    else
+    {
+      line[line_length - 1] = '\0';
+      strcpy(p, line);
+      line_ptr[nr_of_lines++] = p;
+    }
+  }
+
+  return nr_of_lines;
+}
+
+void write_lines(char *line_ptr[], const size_t nr_of_lines)
+{
+  for (size_t i = 0; i < nr_of_lines; ++i)
+  {
+    puts(line_ptr[i]);
+  }
 }
