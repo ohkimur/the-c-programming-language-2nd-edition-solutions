b5c6110e6ff958ab19b55960d789fc80a0f4a3ae
Daniel Costrasel <gleesik.dark@gmail.com>
2021-02-28 21:41:07 +0200

    solution: implemented the base for sort

--- chapter_05/exercise_5_14/sort.c
+++ chapter_05/exercise_5_14/sort.c
@@ -3,12 +3,15 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_LINE_LENGTH 1000
+#define MAX_NR_OF_LINES 5000
 
 int is_arg_list_valid(int argc, char *argv[]);
 
 size_t read_lines(char *line_ptr[], const size_t max_nr_of_lines);
 void write_lines(char *line_ptr[], const size_t nr_of_lines);
+
+void swap(void *v[], size_t i, size_t j);
+void q_sort(void *v[], size_t left, size_t right, int (*comp)(void *, void *));
 
 int main(int argc, char *argv[])
 {
@@ -19,11 +22,12 @@ int main(int argc, char *argv[])
   }
 
   size_t nr_of_lines;
-  char *line_ptr[MAX_LINE_LENGTH];
+  char *line_ptr[MAX_NR_OF_LINES];
 
-  if ((nr_of_lines = read_lines(line_ptr, MAX_LINE_LENGTH)) != -1)
+  if ((nr_of_lines = read_lines(line_ptr, MAX_NR_OF_LINES)) != -1)
   {
     // TODO: Sort the lines.
+    q_sort((void **)line_ptr, 0, nr_of_lines, (int (*)(void *, void *))strcmp);
     write_lines(line_ptr, nr_of_lines);
   }
   else
@@ -45,7 +49,7 @@ size_t read_lines(char *line_ptr[], const size_t max_nr_of_lines)
 {
   size_t line_length;
   size_t nr_of_lines = 0;
-  size_t bufsize = MAX_LINE_LENGTH;
+  size_t bufsize = max_nr_of_lines;
 
   char *current_line = NULL;
   char *current_line_copy = NULL;
@@ -78,4 +82,36 @@ void write_lines(char *line_ptr[], const size_t nr_of_lines)
   {
     puts(line_ptr[i]);
   }
+}
+
+void swap(void *v[], size_t i, size_t j)
+{
+  void *temp;
+  temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
+
+void q_sort(void *v[], size_t left, size_t right, int (*comp)(void *, void *))
+{
+  if (left >= right)
+  {
+    return;
+  }
+
+  size_t last;
+  swap(v, left, (left + right) / 2);
+  last = left;
+
+  for (size_t i = left + 1; i <= right; ++i)
+  {
+    if ((*comp)(v[i], v[left]) < 0)
+    {
+      swap(v, ++last, i);
+    }
+  }
+
+  swap(v, left, last);
+  q_sort(v, left, last - 1, comp);
+  q_sort(v, last + 1, right, comp);
 }
