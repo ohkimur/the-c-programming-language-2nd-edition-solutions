d55014ff2a6106243a34881eb0a7478ce7bcbbe2
Daniel Costrasel <gleesik.dark@gmail.com>
2021-03-09 10:29:52 +0200

    chore: implemented initial version of get_token

---
+++ chapter_05/exercise_5_18/decl.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_TOKEN_LEN 100
+
+int get_token(void);
+
+enum
+{
+  NAME,
+  PARENS,
+  BRACKETS
+};
+
+int token_type;
+
+char token[MAX_TOKEN_LEN];
+char name[MAX_TOKEN_LEN];
+char data_type[MAX_TOKEN_LEN];
+char out[MAX_TOKEN_LEN];
+
+int main(void)
+{
+  while (get_token() != EOF)
+  {
+    printf("%s\t%s\n", token, name);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int get_token(void)
+{
+  int c;
+  char *p = token;
+
+  while ((c = getc(stdin)) == ' ' || c == '\t')
+    ;
+
+  if (c == '(')
+  {
+    if ((c = getc(stdin)) == ')')
+    {
+      strcpy(token, "()");
+      return token_type = PARENS;
+    }
+    else
+    {
+      ungetc(c, stdin);
+      return token_type = '(';
+    }
+  }
+  else if (c == '[')
+  {
+    *p++ = c;
+    while ((*p++ = getc(stdin)) != ']')
+      ;
+    *p = '\0';
+    return token_type = BRACKETS;
+  }
+  else if (isalpha(c))
+  {
+    *p++ = c;
+    while (isalnum(c = getc(stdin)))
+    {
+      *p++ = c;
+    }
+    *p = '\0';
+    ungetc(c, stdin);
+    return token_type = NAME;
+  }
+
+  return token_type = c;
+}
