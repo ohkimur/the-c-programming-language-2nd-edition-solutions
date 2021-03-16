09fd6e4a56ad3fca5dca877ce94410a577eb3b92
Daniel Costrasel <gleesik.dark@gmail.com>
2021-03-16 10:15:44 +0200

    chore: implemented the base count c keywords program

--- chapter_06/exercise_6_1/count_c_keywords.c
+++ chapter_06/exercise_6_1/count_c_keywords.c
@@ -1,8 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_WORD_LEN 100
+#define NR_OF_KEYS sizeof(keytab) / sizeof(keytab[0])
+
+struct key
+{
+  char *word;
+  int count;
+};
+
+int get_word(char *word, int lim);
+int bin_search(char *word, struct key tab[], int n);
+
+struct key keytab[] = {
+    {"auto", 0},
+    {"break", 0},
+    {"case", 0},
+    {"char", 0},
+    {"const", 0},
+    {"continue", 0},
+    {"default", 0},
+    /* ... */
+    {"unsigned", 0},
+    {"void", 0},
+    {"while", 0},
+};
 
 int main(void)
 {
-  puts("Start from here.");
+  int n;
+  char word[MAX_WORD_LEN];
+
+  while (get_word(word, MAX_WORD_LEN) != EOF)
+  {
+    if (isalpha(word[0]))
+    {
+      if ((n = bin_search(word, keytab, NR_OF_KEYS)) >= 0)
+      {
+        keytab[n].count++;
+      }
+    }
+  }
+
+  for (size_t i = 0; i < NR_OF_KEYS; ++i)
+  {
+    if (keytab[i].count)
+    {
+      printf("%4d %s\n", keytab[i].count, keytab[i].word);
+    }
+  }
+
   return EXIT_SUCCESS;
+}
+
+int get_word(char *word, int lim)
+{
+  int c;
+  char *w = word;
+
+  while (isspace(c = getc(stdin)))
+    ;
+
+  if (c != EOF)
+  {
+    *w++ = c;
+  }
+
+  if (!isalpha(c))
+  {
+    *w = '\0';
+    return c;
+  }
+
+  for (; --lim > 0; w++)
+  {
+    if (!isalnum(*w = getc(stdin)))
+    {
+      ungetc(*w, stdin);
+      break;
+    }
+  }
+
+  *w = '\0';
+  return word[0];
+}
+
+int bin_search(char *word, struct key tab[], int n)
+{
+
+  int low = 0;
+  int high = n - 1;
+  int mid;
+
+  while (low <= high)
+  {
+    mid = (low + high) / 2;
+
+    int cond = strcmp(word, tab[mid].word);
+    if (cond < 0)
+    {
+      high = mid - 1;
+    }
+    else if (cond > 0)
+    {
+      low = mid + 1;
+    }
+    else
+    {
+      return mid;
+    }
+  }
+
+  return -1;
 }
