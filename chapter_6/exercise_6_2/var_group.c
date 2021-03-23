c3b8f7dfa613dc6e8aa7987c6f90837fca6b9a3d
Daniel Costrasel <gleesik.dark@gmail.com>
2021-03-23 09:15:07 +0200

    chore: defined tree_node struct

--- chapter_6/exercise_6_2/var_group.c
+++ chapter_6/exercise_6_2/var_group.c
@@ -3,6 +3,16 @@
 #include <string.h>
 #include <ctype.h>
 
+#define MAX_WORD_LEN
+
+struct tree_node
+{
+  char *word;
+  int count;
+  struct tree_node *left;
+  struct tree_node *right;
+};
+
 void skip_blanks();
 void skip_comments();
 void skip_string_between(char start, char end);
