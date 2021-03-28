6f3e75d216aadd1244edbd74e91b117b50266aaa
Daniel Costrasel <gleesik.dark@gmail.com>
2021-03-28 20:38:45 +0300

    chore: removed note

--- chapter_1/exercise_1_19/reverse.c
+++ chapter_1/exercise_1_19/reverse.c
@@ -69,8 +69,3 @@ void reverse(char line[])
     --i_back;
   }
 }
-
-// NOTE: It is more optimized to not use a temporary copy of the entire char
-// array. The reverse function needs just one parameter, so there is necessary
-// to write a function that determine de length of the string. In this case that
-// function is reverse().
