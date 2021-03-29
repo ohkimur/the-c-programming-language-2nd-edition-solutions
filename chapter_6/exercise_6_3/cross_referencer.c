69eafeca12d20d295c91da66609fccbb08c0cd4c
Daniel Costrasel <gleesik.dark@gmail.com>
2021-03-29 14:55:52 +0300

    feature: store line numbers in single linked list

--- chapter_6/exercise_6_3/cross_referencer.c
+++ chapter_6/exercise_6_3/cross_referencer.c
@@ -6,24 +6,31 @@
 #define MAX_WORD_LEN 100
 #define NR_OF_TYPES sizeof(data_types) / sizeof(data_types[0])
 
-struct tree_node
+enum boolean
 {
-  char *word;
-  struct tree_node *left;
-  struct tree_node *right;
+  FALSE,
+  TRUE
 };
 
 struct list_node
 {
-  struct tree_node *var_group;
+  size_t line_number;
   struct list_node *next;
 };
 
+struct tree_node
+{
+  char *word;
+  struct list_node *line_numbers;
+  struct tree_node *left;
+  struct tree_node *right;
+};
+
+struct list_node *add_to_list(struct list_node *list_node_p, size_t line_number);
+void print_list(struct list_node *node_p);
+
 struct tree_node *add_to_tree(struct tree_node *node_p, char *word);
 void print_tree(struct tree_node *node_p);
-
-struct list_node *add_to_list(struct list_node *list_node_p, char *word);
-void print_list(struct list_node *node_p);
 
 // There is a strdup available with POSIX, but it's not part of ISO C.
 char *str_dup(char *src);
@@ -32,20 +39,28 @@ void skip_blanks();
 
 int get_word(char *word, int max_word_len);
 
+size_t line_number = 1;
+
 int main(void)
 {
-  struct list_node *list_root = NULL;
+  int c;
+  struct tree_node *tree_root = NULL;
   char word[MAX_WORD_LEN];
 
-  while (get_word(word, MAX_WORD_LEN) != EOF)
+  while ((c = get_word(word, MAX_WORD_LEN)) != EOF)
   {
+    if (c == '\n')
+    {
+      ++line_number;
+    }
+
     if (isalpha(word[0]))
     {
-      list_root = add_to_list(list_root, word);
+      tree_root = add_to_tree(tree_root, word);
     }
   }
 
-  print_list(list_root);
+  print_tree(tree_root);
 
   return EXIT_SUCCESS;
 }
@@ -151,19 +166,21 @@ struct tree_node *add_to_tree(struct tree_node *node_p, char *word)
   if (node_p == NULL)
   {
     node_p = (struct tree_node *)malloc(sizeof(struct tree_node));
+    node_p->line_numbers = add_to_list(node_p->line_numbers, line_number);
     node_p->word = str_dup(word);
     node_p->left = node_p->right = NULL;
   }
-  else if ((cond = strcmp(word, node_p->word)) != 0)
+  else if ((cond = strcmp(word, node_p->word)) == 0)
   {
-    if (cond < 0)
-    {
-      node_p->left = add_to_tree(node_p->left, word);
-    }
-    else if (cond > 0)
-    {
-      node_p->right = add_to_tree(node_p->right, word);
-    }
+    node_p->line_numbers = add_to_list(node_p->line_numbers, line_number);
+  }
+  else if (cond < 0)
+  {
+    node_p->left = add_to_tree(node_p->left, word);
+  }
+  else if (cond > 0)
+  {
+    node_p->right = add_to_tree(node_p->right, word);
   }
 
   return node_p;
@@ -174,25 +191,24 @@ void print_tree(struct tree_node *node_p)
   if (node_p != NULL)
   {
     print_tree(node_p->left);
-    puts(node_p->word);
+    printf("%s: ", node_p->word);
+    print_list(node_p->line_numbers);
+    putchar('\n');
     print_tree(node_p->right);
   }
 }
 
-struct list_node *add_to_list(struct list_node *list_node_p, char *word)
+struct list_node *add_to_list(struct list_node *list_node_p, size_t line_number)
 {
   if (list_node_p == NULL)
   {
     list_node_p = (struct list_node *)malloc(sizeof(struct list_node));
-    list_node_p->var_group = add_to_tree(list_node_p->var_group, word);
-  }
-  else if (strcmp(list_node_p->var_group->word, word) == 0)
-  {
-    list_node_p->var_group = add_to_tree(list_node_p->var_group, word);
+    list_node_p->line_number = line_number;
+    list_node_p->next = NULL;
   }
   else
   {
-    list_node_p->next = add_to_list(list_node_p->next, word);
+    list_node_p->next = add_to_list(list_node_p->next, line_number);
   }
 
   return list_node_p;
@@ -200,11 +216,24 @@ struct list_node *add_to_list(struct list_node *list_node_p, char *word)
 
 void print_list(struct list_node *node_p)
 {
+  static enum boolean first = TRUE;
   if (node_p != NULL)
   {
-    print_tree(node_p->var_group);
-    putchar('\n');
+    if (first)
+    {
+      first = FALSE;
+      printf("%zu", node_p->line_number);
+    }
+    else
+    {
+      printf(", %zu", node_p->line_number);
+    }
+
     print_list(node_p->next);
+  }
+  else
+  {
+    first = TRUE;
   }
 }
 
