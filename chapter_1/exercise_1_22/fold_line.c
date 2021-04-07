ddd5eb6d477e51e5ed3dfb44b3b7af8acb2e6f4f
Luca Airinei <illustrofia@gmail.com>
2021-04-07 14:41:32 +0300

    Update fold_line program to meet exercise requirements.

--- chapter_1/exercise_1_22/fold_line.c
+++ chapter_1/exercise_1_22/fold_line.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define MAXLINE 1000
+#define MAXLINE 10000
 
 #define TRUE (1 == 1)
 #define FALSE !TRUE
@@ -13,11 +13,11 @@ int main(void)
   char line[MAXLINE];
   char fold_str[MAXLINE];
 
-  get_line(line, MAXLINE);
-
-  fold_line(line, fold_str, 30);
-
-  printf("%s", fold_str);
+  while ((get_line(line, MAXLINE)) > 0)
+  {
+    fold_line(line, fold_str, 40);
+    printf("%s", fold_str);
+  }
 
   return 0;
 }
@@ -43,30 +43,43 @@ int get_line(char line[], int max_line_len)
 
 void fold_line(char line[], char fold_str[], int n_break)
 {
-  int i, j;
+  int i, j, m;
+  int colon = 0;
   int split = FALSE;
-  int offset = 10;
 
-  for (i = 0, j = 0; line[i] != '\0'; ++i, ++j)
+  for (i = 0, j = 0; line[i] != '\0'; i++, j++)
   {
-    if (i && i % n_break == 0)
+    fold_str[j] = line[i];
+
+    if (fold_str[j] == '\n')
+    {
+      colon = 0;
+    }
+
+    colon++;
+
+    if (colon == n_break)
     {
       split = TRUE;
+      for (m = j; m >= j - 10 && split; m--)
+      {
+        if (fold_str[m] == ' ' || fold_str[m] == '\t')
+        {
+          fold_str[m] = '\n';
+          colon = j - m;
+
+          split = FALSE;
+        }
+      }
+
+      if (split)
+      {
+        fold_str[j++] = '-';
+        fold_str[j] = '\n';
+
+        colon = 0;
+      }
     }
-
-    if (split && (line[i] == ' ' || i > n_break + offset))
-    {
-      fold_str[j++] = '\n';
-      split = FALSE;
-      ++i;
-    }
-
-    fold_str[j] = line[i];
-  }
-
-  if (line[i] == '\n')
-  {
-    fold_str[j++] = line[i];
   }
 
   fold_str[j] = '\0';
