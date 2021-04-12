bf7caacc59c93030be0f3eb3078439c09a0a8b63
Daniel Costrasel <gleesik.dark@gmail.com>
2021-04-12 11:21:15 +0300

    chore: added get_word and adapted for this use case

--- chapter_6/exercise_6_6/define.c
+++ chapter_6/exercise_6_6/define.c
@@ -5,6 +5,7 @@
 
 #define HASH_SIZE 101
 #define MAX_LINE_LEN 1000
+#define MAX_WORD_LEN 100
 
 enum boolean
 {
@@ -27,14 +28,26 @@ struct list_node *lookup(char *str);
 struct list_node *install(char *name, char *definition);
 enum boolean undef(char *name);
 
+int get_word(char *word, int max_word_len);
+
 char *test = "#define MAX";
 
 static struct list_node *hash_table[HASH_SIZE];
 
 int main(void)
 {
-  // NOTE: The best approach might be using something like get_next_token.
-  // However, in that case the space has to be considered a token.
+  char word[MAX_WORD_LEN];
+  while (get_word(word, MAX_WORD_LEN) != EOF)
+  {
+    if (isalpha(word[0]))
+    {
+      printf("%s", word);
+    }
+    else
+    {
+      putchar(word[0]);
+    }
+  }
 
   return EXIT_SUCCESS;
 }
@@ -129,4 +142,30 @@ enum boolean undef(char *name)
   }
 
   return FALSE;
+}
+
+int get_word(char *word, int max_word_len)
+{
+  int c = getc(stdin);
+  size_t i = 0;
+
+  if (c != EOF)
+  {
+    word[i++] = c;
+  }
+
+  if (!isalpha(c) && c != '_')
+  {
+    word[i] = '\0';
+    return c;
+  }
+
+  while ((isalnum(c = getc(stdin)) || c == '_') && i < max_word_len)
+  {
+    word[i++] = c;
+  }
+  ungetc(c, stdin);
+  word[i] = '\0';
+
+  return word[0];
 }
