fac2cd1db7037a4ba83949500e89ca5fe4c0e4e1
Daniel Costrasel <gleesik.dark@gmail.com>
2021-04-20 15:14:57 +0300

    chore: implemented boolean type and parse_arg_list

--- chapter_7/exercise_7_1/case.c
+++ chapter_7/exercise_7_1/case.c
@@ -3,8 +3,40 @@
 #include <string.h>
 #include <ctype.h>
 
+typedef enum __BOOL
+{
+  FALSE,
+  TRUE
+} boolean;
+
+boolean lower = TRUE;
+boolean parse_arg_list(int argc, char *argv[]);
+
 int main(int argc, char *argv[])
 {
-  puts("Silence is golden.");
+  if (!parse_arg_list(argc, argv))
+  {
+    puts("Error: invalid arguments.");
+    return EXIT_FAILURE;
+  }
+
+  printf("%s\n", lower ? "lower" : "upper");
+
   return EXIT_SUCCESS;
+}
+
+boolean parse_arg_list(int argc, char *argv[])
+{
+  if (strcmp(argv[0], "lower") == 0)
+  {
+    lower = TRUE;
+    return FALSE;
+  }
+  else if (strcmp(argv[0], "upper") == 0)
+  {
+    lower = FALSE;
+    return FALSE;
+  }
+
+  return TRUE;
 }
