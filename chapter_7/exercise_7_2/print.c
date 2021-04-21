991e6aa55f61ada79dd412063589b4d04dbd0ca0
Daniel Costrasel <gleesik.dark@gmail.com>
2021-04-21 23:00:41 +0300

    chore: created the base for 7-2

---
+++ chapter_7/exercise_7_2/print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+  // Before changing the world, do this.
+  return EXIT_SUCCESS;
+}
