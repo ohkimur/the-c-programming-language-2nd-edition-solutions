4171e458fe345efc957d7987301477c355e604ab
Daniel Costrasel <gleesik.dark@gmail.com>
2021-04-23 13:45:12 +0300

    chore: implemented is_ascii

--- chapter_7/exercise_7_2/print.c
+++ chapter_7/exercise_7_2/print.c
@@ -3,14 +3,33 @@
 #include <string.h>
 #include <ctype.h>
 
+int is_ascii(int c);
+
 int main(int argc, char *argv[])
 {
   // Before changing the world, do this.
   int c;
   while ((c = getc(stdin)) != EOF)
   {
-    putc(c, stdout);
+    if (is_ascii(c))
+    {
+      putc(c, stdout);
+    }
+    else
+    {
+      putc('#', stdout);
+    }
   }
 
   return EXIT_SUCCESS;
+}
+
+int is_ascii(int c)
+{
+  if (c > 127)
+  {
+    return 0;
+  }
+
+  return 1;
 }
