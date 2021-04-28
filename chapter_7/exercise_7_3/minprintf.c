d71fde274895d86b9188fccdfe001c54ae27741b
Daniel Costrasel <gleesik.dark@gmail.com>
2021-04-28 14:05:50 +0300

    feature: added support for octal number

--- chapter_7/exercise_7_3/minprintf.c
+++ chapter_7/exercise_7_3/minprintf.c
@@ -6,7 +6,7 @@ void minprintf(const char *fmt, ...);
 
 int main(int argc, char *argv[])
 {
-  minprintf("This is a test. Let's print %d, and %f, and %s.\n", 2, 3.14159, "hello, world");
+  minprintf("This is a test. Let's print %d, %i, %o, and %f, and %s.\n", 2, 3, 7, 3.14159, "hello, world");
   return EXIT_SUCCESS;
 }
 
@@ -26,7 +26,12 @@ void minprintf(const char *fmt, ...)
     switch (*++fmt)
     {
     case 'd':
+    case 'i':
       printf("%d", va_arg(ap, int));
+      break;
+
+    case 'o':
+      printf("%o", va_arg(ap, int));
       break;
 
     case 'f':
