5be17401dae743805d865dd01cd5dad2275ea259
Daniel Costrasel <gleesik.dark@gmail.com>
2021-04-28 15:10:02 +0300

    refactor: improved var name

--- chapter_7/exercise_7_3/minprintf.c
+++ chapter_7/exercise_7_3/minprintf.c
@@ -14,9 +14,9 @@ int main(int argc, char *argv[])
 
 void minprintf(const char *format, ...)
 {
-  va_list ap;
+  va_list arg_p;
 
-  va_start(ap, format);
+  va_start(arg_p, format);
   for (; *format != '\0'; ++format)
   {
     if (*format != '%')
@@ -29,46 +29,46 @@ void minprintf(const char *format, ...)
     {
     case 'd':
     case 'i':
-      printf("%d", va_arg(ap, int));
+      printf("%d", va_arg(arg_p, int));
       break;
 
     case 'o':
-      printf("%o", va_arg(ap, int));
+      printf("%o", va_arg(arg_p, int));
       break;
 
     case 'x':
     case 'X':
-      printf("%x", va_arg(ap, int));
+      printf("%x", va_arg(arg_p, int));
       break;
 
     case 'u':
-      printf("%u", va_arg(ap, int));
+      printf("%u", va_arg(arg_p, int));
       break;
 
     case 'c':
-      printf("%c", va_arg(ap, int));
+      printf("%c", va_arg(arg_p, int));
       break;
 
     case 's':
-      printf("%s", va_arg(ap, char *));
+      printf("%s", va_arg(arg_p, char *));
       break;
 
     case 'f':
-      printf("%f", va_arg(ap, double));
+      printf("%f", va_arg(arg_p, double));
       break;
 
     case 'e':
     case 'E':
-      printf("%e", va_arg(ap, double));
+      printf("%e", va_arg(arg_p, double));
       break;
 
     case 'g':
     case 'G':
-      printf("%g", va_arg(ap, double));
+      printf("%g", va_arg(arg_p, double));
       break;
 
     case 'p':
-      printf("%p", va_arg(ap, void *));
+      printf("%p", va_arg(arg_p, void *));
       break;
 
     default:
@@ -76,5 +76,5 @@ void minprintf(const char *format, ...)
       break;
     }
   }
-  va_end(ap);
+  va_end(arg_p);
 }
