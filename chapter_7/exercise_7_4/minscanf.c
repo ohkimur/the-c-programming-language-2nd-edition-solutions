5c3464433da0c6c8e50aea75215feaf694aa1e26
Daniel Costrasel <gleesik.dark@gmail.com>
2021-05-07 15:31:42 +0300

    fix: added support for input data argument type

--- chapter_7/exercise_7_4/minscanf.c
+++ chapter_7/exercise_7_4/minscanf.c
@@ -33,6 +33,36 @@ void minscanf(const char *format, ...)
       scanf("%d", va_arg(arg_p, int *));
       break;
 
+    case 'i':
+      scanf("%i", va_arg(arg_p, int *));
+      break;
+
+    case 'o':
+      scanf("%o", va_arg(arg_p, int *));
+      break;
+
+    case 'u':
+      scanf("%u", va_arg(arg_p, unsigned int *));
+      break;
+
+    case 'x':
+      scanf("%x", va_arg(arg_p, int *));
+      break;
+
+    case 'c':
+      scanf("%c", va_arg(arg_p, char *));
+      break;
+
+    case 's':
+      scanf("%s", va_arg(arg_p, char *));
+      break;
+
+    case 'e':
+    case 'f':
+    case 'g':
+      scanf("%f", va_arg(arg_p, float *));
+      break;
+
     default:
       break;
     }
