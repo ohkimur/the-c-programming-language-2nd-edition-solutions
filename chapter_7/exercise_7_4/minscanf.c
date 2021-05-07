8f55dea0bbc8c60ef160a41cbeb71325952e6c85
Daniel Costrasel <gleesik.dark@gmail.com>
2021-05-07 15:28:09 +0300

    feature: implemented the base for minscanf

--- chapter_7/exercise_7_4/minscanf.c
+++ chapter_7/exercise_7_4/minscanf.c
@@ -2,7 +2,40 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+void minscanf(const char *format, ...);
+
 int main(int argc, char *argv[])
 {
+  int a = 0;
+  int b = 0;
+
+  minscanf("%d %d", &a, &b);
+  printf("a: %d, b: %d\n", a, b);
+
   return EXIT_SUCCESS;
+}
+
+void minscanf(const char *format, ...)
+{
+  va_list arg_p;
+
+  va_start(arg_p, format);
+  for (; *format != '\0'; ++format)
+  {
+    if (*format != '%')
+    {
+      continue;
+    }
+
+    switch (*++format)
+    {
+    case 'd':
+      scanf("%d", va_arg(arg_p, int *));
+      break;
+
+    default:
+      break;
+    }
+  }
+  va_end(arg_p);
 }
