a15232abab74cf761dfc607bf5d3f2f3597dbfce
Daniel Costrasel <gleesik.dark@gmail.com>
2021-05-08 20:25:31 +0300

    chore: ported stack + push & pop functions

--- chapter_7/exercise_7_5/calculator.c
+++ chapter_7/exercise_7_5/calculator.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define STACK_MAX_SIZE 100
+
+void push(double);
+double pop(void);
+
+int sp = 0;
+double stack[STACK_MAX_SIZE];
+
 int main(int argc, char *argv[])
 {
   return EXIT_SUCCESS;
+}
+
+void push(double f)
+{
+  if (sp < STACK_MAX_SIZE)
+  {
+    stack[sp++] = f;
+  }
+  else
+  {
+    printf("error: stack full, can't push %g\n", f);
+  }
+}
+
+double pop(void)
+{
+  if (sp > 0)
+  {
+    return stack[--sp];
+  }
+  else
+  {
+    printf("error: stack empty\n");
+    return 0.0;
+  }
 }
