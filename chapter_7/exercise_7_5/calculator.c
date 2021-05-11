d6f97f58d15556958993894e9589f63d8d30f4e6
Daniel Costrasel <gleesik.dark@gmail.com>
2021-05-11 13:22:12 +0300

    chore: removed getop

--- chapter_7/exercise_7_5/calculator.c
+++ chapter_7/exercise_7_5/calculator.c
@@ -4,81 +4,75 @@
 
 #define STACK_MAX_SIZE 100
 
-#define MAXOP 100
-#define NUMBER '0'
-
 void push(double);
 double pop(void);
-int getop(double *number);
 
 int sp = 0;
 double stack[STACK_MAX_SIZE];
 
 int main(int argc, char *argv[])
 {
-  int type;
   double op2;
-  char s[MAXOP];
 
   // TODO: Modify getop to read numbers from input and pass them directly to the caller. Keep the
   // type return, that will be useful if it's necessary to scale the program.
-  while ((type = getop(&op2)) != EOF)
-  {
-    switch (type)
-    {
-    case NUMBER:
-      push(op2);
-      break;
+  // while ((type = getop(&op2)) != EOF)
+  // {
+  //   switch (type)
+  //   {
+  //   case NUMBER:
+  //     push(op2);
+  //     break;
 
-    case '+':
-      push(pop() + pop());
-      break;
+  //   case '+':
+  //     push(pop() + pop());
+  //     break;
 
-    case '-':
-      op2 = pop();
-      push(pop() - op2);
-      break;
+  //   case '-':
+  //     op2 = pop();
+  //     push(pop() - op2);
+  //     break;
 
-    case '*':
-      push(pop() * pop());
-      break;
+  //   case '*':
+  //     push(pop() * pop());
+  //     break;
 
-    case '/':
-      op2 = pop();
+  //   case '/':
+  //     op2 = pop();
 
-      if (op2 != 0.0)
-      {
-        push(pop() / op2);
-      }
-      else
-      {
-        printf("error: zero divisor\n");
-      }
+  //     if (op2 != 0.0)
+  //     {
+  //       push(pop() / op2);
+  //     }
+  //     else
+  //     {
+  //       printf("error: zero divisor\n");
+  //     }
 
-      break;
+  //     break;
 
-    case '%':
-      op2 = pop();
+  //   case '%':
+  //     op2 = pop();
 
-      if (op2 != 0.0)
-      {
-        push((int)pop() % (int)op2);
-      }
-      else
-      {
-        printf("error: zero divisor\n");
-      }
-      break;
+  //     if (op2 != 0.0)
+  //     {
+  //       push((int)pop() % (int)op2);
+  //     }
+  //     else
+  //     {
+  //       printf("error: zero divisor\n");
+  //     }
+  //     break;
 
-    case '\n':
-      printf("result: %.8g\n", pop());
-      break;
+  //   case '\n':
+  //     printf("result: %.8g\n", pop());
+  //     break;
 
-    default:
-      printf("error: unknown command %s\n", s);
-      break;
-    }
-  }
+  //   default:
+  //     printf("error: unknown command %s\n", s);
+  //     break;
+  //   }
+  // }
   return EXIT_SUCCESS;
 }
 
@@ -105,54 +99,4 @@ double pop(void)
     printf("error: stack empty\n");
     return 0.0;
   }
-}
-
-int getop(double *number)
-{
-  int i = 0, c;
-
-  while ((s[0] = c = getc(stdin)) == ' ' || c == '\t')
-    ;
-
-  s[1] = '\0';
-
-  if (!isdigit(c) && c != '.' && c != '-')
-  {
-    return c;
-  }
-
-  if (c == '-')
-  {
-    int next = getc(stdin);
-    if (!isdigit(next) && next != '.')
-    {
-      return next;
-    }
-
-    s[i] = c;
-    ungetc(c = next, stdin);
-  }
-  else
-  {
-    c = getc(stdin);
-  }
-
-  if (isdigit(c))
-  {
-    while (isdigit(s[++i] = c = getc(stdin)))
-      ;
-  }
-
-  if (c == '.')
-  {
-    while (isdigit(s[++i] = c = getc(stdin)))
-      ;
-  }
-
-  if (c != EOF)
-  {
-    ungetc(c, stdin);
-  }
-
-  return NUMBER;
 }
