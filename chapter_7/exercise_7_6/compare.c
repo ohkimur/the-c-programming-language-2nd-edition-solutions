67386a70561da72fb69316adfcc117efe7ffab39
Daniel Costrasel <gleesik.dark@gmail.com>
2021-05-17 14:25:30 +0300

    feature: added error checking

--- chapter_7/exercise_7_6/compare.c
+++ chapter_7/exercise_7_6/compare.c
@@ -7,7 +7,24 @@ int main(int argc, char *argv[])
 {
   if (!parse_arg_list(argc, argv))
   {
-    puts("Error: invalid arguments.");
+    fprintf(stderr, "Error: invalid arguments.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char *program_name = argv[0];
+
+  FILE *file_1;
+  FILE *file_2;
+
+  if ((file_1 = fopen(argv[1], "r")) == NULL)
+  {
+    fprintf(stderr, "%s: can't open %s.\n", program_name, argv[1]);
+    exit(EXIT_FAILURE);
+  }
+
+  if ((file_2 = fopen(argv[2], "r")) == NULL)
+  {
+    fprintf(stderr, "%s: can't open %s.\n", program_name, argv[2]);
     exit(EXIT_FAILURE);
   }
 
