cff294b7c35f9e1f2d60285050ef632c85cb1c68
Daniel Costrasel <gleesik.dark@gmail.com>
2021-05-17 14:18:50 +0300

    chore: check arguments

--- chapter_7/exercise_7_6/compare.c
+++ chapter_7/exercise_7_6/compare.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int parse_arg_list(int argc, char *argv[]);
+
 int main(int argc, char *argv[])
 {
-  return EXIT_SUCCESS;
+  if (!parse_arg_list(argc, argv))
+  {
+    puts("Error: invalid arguments.");
+    exit(EXIT_FAILURE);
+  }
+
+  exit(EXIT_SUCCESS);
+}
+
+int parse_arg_list(int argc, char *argv[])
+{
+  if (argc < 3)
+  {
+    return 0;
+  }
+
+  return 1;
 }
