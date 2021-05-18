058418ef8976c9b38739297aa7645145766a87b2
Daniel Costrasel <gleesik.dark@gmail.com>
2021-05-18 15:03:27 +0300

    chore: written the base find program from chapter 5

---
+++ chapter_7/exercise_7_7/find.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LEN 1000
+
+int parse_arg_list(int argc, char *argv[]);
+
+int except = 0;
+int number = 0;
+int found = 0;
+
+int main(int argc, char *argv[])
+{
+  if (!parse_arg_list(argc, argv))
+  {
+    exit(EXIT_FAILURE);
+  }
+
+  char line[MAX_LINE_LEN];
+  size_t line_number = 0;
+  while (fgets(line, MAX_LINE_LEN, stdin) != NULL)
+  {
+    ++line_number;
+    if ((strstr(line, *argv) != NULL) != except)
+    {
+      if (number)
+      {
+        printf("%ld: ", line_number);
+      }
+      printf("%s", line);
+      ++found;
+    }
+  }
+
+  exit(EXIT_SUCCESS);
+}
+
+int parse_arg_list(int argc, char *argv[])
+{
+  const char *program_name = argv[0];
+
+  while (--argc > 0 && (*++argv)[0] == '-')
+  {
+    int c;
+    while ((c = *++argv[0]))
+    {
+      switch (c)
+      {
+      case 'x':
+        except = 1;
+        break;
+
+      case 'n':
+        number = 1;
+        break;
+
+      default:
+        fprintf(stderr, "%s: illegal option %c.\n", program_name, c);
+        argc = 0;
+        found = -1;
+        break;
+      }
+    }
+  }
+
+  if (argc != 1)
+  {
+    printf("Usage: find -x -n pattern.\n");
+    return 0;
+  }
+
+  return 1;
+}
--- chapter_7/exercise_7_7/pattern.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[])
-{
-  exit(EXIT_SUCCESS);
-}
