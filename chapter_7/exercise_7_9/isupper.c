c4e8429057520a4b7cb2cb93077aba2e9650c13c
Daniel Costrasel <gleesik.dark@gmail.com>
2021-05-21 12:42:00 +0300

    chore: created base for 7-9

---
+++ chapter_7/exercise_7_9/isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LEN 1000
+#define LINES_PER_PAGE 10
+
+typedef enum
+{
+  false,
+  true
+} boolean;
+
+int main(int argc, char *argv[])
+{
+  exit(EXIT_SUCCESS);
+}
