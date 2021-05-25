33cf8724aa89dc4afa5b0fa32ad7869ea4f280dd
Daniel Costrasel <gleesik.dark@gmail.com>
2021-05-25 22:44:25 +0300

    solution: created another custom version for is_upper

--- chapter_7/exercise_7_9/isupper.c
+++ chapter_7/exercise_7_9/isupper.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-int is_upper(int c);
+int is_upper_v1(int c);
+int is_upper_v2(int c);
 
 int main(int argc, char *argv[])
 {
-  if (is_upper('c'))
+  if (is_upper_v1('c'))
   {
-    puts("This letter is uppercase.");
+    puts("is_upper_v1: This letter is uppercase.");
   }
   else
   {
-    puts("This letter is lowercase.");
+    puts("is_upper_v1: This letter is lowercase.");
+  }
+
+  if (is_upper_v2('c'))
+  {
+    puts("is_upper_v2: This letter is uppercase.");
+  }
+  else
+  {
+    puts("is_upper_v2: This letter is lowercase.");
   }
 
   exit(EXIT_SUCCESS);
 }
 
-int is_upper(int c)
+int is_upper_v1(int c)
 {
   return (c >= 'A' && c <= 'Z');
+}
+
+int is_upper_v2(int c)
+{
+  return (strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZ", c) != NULL);
 }
