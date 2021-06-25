38aa84daacfb8a384f756a8fdda196c20f266cb6
Daniel Costrasel <ohkimur@gmail.com>
2021-06-25 12:01:18 +0300

    chore: added todo checklist

--- chapter_8/exercise_8_01/cat.c
+++ chapter_8/exercise_8_01/cat.c
@@ -3,5 +3,13 @@
 
 int main(int argc, char *argv[])
 {
+  /**
+   * TODO:
+   * - [ ] Check if the there are multiple file names as args 
+   *    - [ ] If not, copy the content from stdin to stdout
+   *    - [ ] If yes, open and copy each file content to stdout
+   * - [ ] Make sure there are error checking at every step
+   */
+
   exit(EXIT_SUCCESS);
 }
