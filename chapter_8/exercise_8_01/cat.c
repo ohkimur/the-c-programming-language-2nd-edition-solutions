fb0157471f9bc1fc6ed5ba6175d7f882e888a033
Daniel Costrasel <ohkimur@gmail.com>
2021-06-25 14:08:35 +0300

    solution: implemented cat with read, write, open, close

--- chapter_8/exercise_8_01/cat.c
+++ chapter_8/exercise_8_01/cat.c
@@ -1,15 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+void error(char *format, ...);
+void copy_file(int from, int to);
 
 int main(int argc, char *argv[])
 {
-  /**
-   * TODO:
-   * - [ ] Check if the there are multiple file names as args 
-   *    - [ ] If not, copy the content from stdin to stdout
-   *    - [ ] If yes, open and copy each file content to stdout
-   * - [ ] Make sure there are error checking at every step
-   */
+  if (argc == 1)
+  {
+    copy_file(0, 1);
+  }
+  else
+  {
+    for (int file_index = 0; file_index < argc; ++file_index)
+    {
+      int file_descriptor;
+      if ((file_descriptor = open(argv[file_index], O_RDONLY, 0)) == -1)
+      {
+        error("Error: could not open the file %s.", argv[file_index]);
+        exit(EXIT_FAILURE);
+      }
+
+      copy_file(file_descriptor, 1);
+    }
+  }
 
   exit(EXIT_SUCCESS);
+}
+
+void copy_file(int from, int to)
+{
+  char buffer[BUFFER_SIZE];
+
+  int n;
+  while ((n = read(from, buffer, BUFFER_SIZE)) > 0)
+  {
+    write(to, buffer, n);
+  }
+}
+
+void error(char *format, ...)
+{
+  va_list arg_p;
+
+  va_start(arg_p, format);
+  fprintf(stderr, "Error: ");
+  vfprintf(stderr, format, arg_p);
+  fprintf(stderr, "\n");
+  va_end(arg_p);
+
+  exit(EXIT_FAILURE);
 }
