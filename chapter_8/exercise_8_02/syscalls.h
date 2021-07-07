0cd338b92acbc65627e65ea7a58b7a293e644494
Daniel Costrasel <ohkimur@gmail.com>
2021-07-07 22:59:59 +0300

    chore: created all syscalls defines

--- chapter_8/exercise_8_02/syscalls.h
+++ chapter_8/exercise_8_02/syscalls.h
@@ -19,3 +19,25 @@ extern FILE _io_buffer[MAX_NR_OF_OPEN_FILES];
 #define stdin (&_io_buffer[0])
 #define stdout (&_io_buffer[1])
 #define stderr (&_io_buffer[2])
+
+enum _flags
+{
+  _READ = 01,
+  _WRITE = 02,
+  _UNBUF = 04,
+  _EOF = 010,
+  _ERR = 020
+};
+
+int _fill_buffer(FILE *);
+int _flush_buffer(FILE *);
+
+#define feof(p) ((p->flag & _EOF) != 0)
+#define ferror(p) ((p->flag & _ERR) != 0)
+#define fileno(p) ((p->file_descriptor)
+
+#define getc(p) (--p->cnt >= 0) ? (unsigned char) *(p)->next_char_pos_p++ : _fill_buffer(p))
+#define putc(x, p) (--p->cnt >= 0) ? *p->next_char_pos_p++ = x : _flush_buffer(x,p))
+
+#define getchar() getc(stdin)
+#define putchar(x) putc(x, stdout)
