1b4b39f8e6bfebdecfcc5324af9d658745d98d6f
Daniel Costrasel <ohkimur@gmail.com>
2021-07-16 12:19:11 +0300

    chore: added prototypes to the header

--- chapter_8/exercise_8_03/syscalls.h
+++ chapter_8/exercise_8_03/syscalls.h
@@ -29,6 +29,9 @@ extern FILE _io_buffer[MAX_NR_OF_OPEN_FILES];
 
 int _fill_buffer(FILE *);
 int _flush_buffer(int c, FILE *file_p);
+int file_flush(FILE *file_p);
+FILE *file_open(char *name, char *mode);
+int file_close(FILE *file_p);
 
 #define stdin (&_io_buffer[0])
 #define stdout (&_io_buffer[1])
