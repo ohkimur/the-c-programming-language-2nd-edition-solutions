875b0a5a35cfd3aeed55d98136ee7a2e5f7873cb
Daniel Costrasel <ohkimur@gmail.com>
2021-07-25 20:46:05 +0300

    chore: implemented dir_walk

--- chapter_8/exercise_8_05/fsize.c
+++ chapter_8/exercise_8_05/fsize.c
@@ -44,3 +44,37 @@ void fsize(char *name)
 
   printf("%s: %ld\n", name, (long)buffer.st_size);
 }
+
+void dir_walk(char *dir_name, void (*func)(char *))
+{
+  char name[MAX_PATH_LEN];
+  struct dirent *dir_entry;
+  DIR *dir;
+
+  if ((dir = opendir(dir_name)) == NULL)
+  {
+    fprintf(stderr, "dir_walk: cannot open %s\n", dir_name);
+    return;
+  }
+
+  while ((dir_entry = readdir(dir)) != NULL)
+  {
+    if (strcmp(dir_entry->d_name, ".") == 0 ||
+        strcmp(dir_entry->d_name, "..") == 0)
+    {
+      continue; // Skip self and parent.
+    }
+
+    if (strlen(dir_name) + strlen(dir_entry->d_name) + 2 > MAX_PATH_LEN)
+    {
+      fprintf(stderr, "dir_walk: path too long\n");
+    }
+    else
+    {
+      sprintf(name, "%s/%s", dir_name, dir_entry->d_name);
+      (*func)(name);
+    }
+
+    closedir(dir);
+  }
+}
