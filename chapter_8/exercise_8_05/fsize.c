e933502e56151309fe346137905bf1711c711f98
Daniel Costrasel <ohkimur@gmail.com>
2021-07-27 21:31:35 +0300

    feature: print fil size

--- chapter_8/exercise_8_05/fsize.c
+++ chapter_8/exercise_8_05/fsize.c
@@ -4,11 +4,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <time.h>
 
 #define MAX_PATH_LEN 1024
 
 void fsize(char *name);
 void dir_walk(char *dir_name, void (*func)(char *));
+void print_file_size(size_t size);
 
 int main(int argc, char *argv[])
 {
@@ -43,36 +45,41 @@ void fsize(char *name)
   }
 
   /**
-   * dev_t st_dev;         [XSI] ID of device containing file
-   * ino_t st_ino;         [XSI] File serial number
-   * mode_t st_mode;       [XSI] Mode of file (see below)
-   * nlink_t st_nlink;     [XSI] Number of hard links
-   * uid_t st_uid;         [XSI] User ID of the file
-   * gid_t st_gid;         [XSI] Group ID of the file
-   * dev_t st_rdev;        [XSI] Device ID
-   * time_t st_atime;      [XSI] Time of last access
-   * time_t st_mtime;      [XSI] Last data modification time
-   * time_t st_ctime;      [XSI] Time of last status change
-   * off_t st_size;        [XSI] File size, in bytes
-   * blkcnt_t st_blocks;   [XSI] Blocks allocated for file
-   * blksize_t st_blksize; [XSI] Optimal blocksize for I/O
+   * off_t st_size         File size, in bytes
+   * dev_t st_dev          ID of device containing file
+   * ino_t st_ino          File serial number
+   * mode_t st_mode        Mode of file (see below)
+   * nlink_t st_nlink      Number of hard links
+   * uid_t st_uid          User ID of the file
+   * gid_t st_gid          Group ID of the file
+   * dev_t st_rdev         Device ID
+   * time_t st_atime       Time of last access
+   * time_t st_mtime       Last data modification time
+   * time_t st_ctime       Time of last status change
+   * blkcnt_t st_blocks    Blocks allocated for file
+   * blksize_t st_blksize  Optimal blocksize for I/O
    */
 
-  printf("%-50s: ", name);
-  printf("%-6lld ", buffer.st_size);
-  printf("%d ", buffer.st_dev);
-  printf("%llu ", buffer.st_ino);
-  printf("%d ", buffer.st_mode);
-  printf("%d ", buffer.st_nlink);
-  printf("%d ", buffer.st_uid);
-  printf("%d ", buffer.st_gid);
-  printf("%d ", buffer.st_rdev);
-  printf("%ld ", buffer.st_atime);
-  printf("%ld ", buffer.st_mtime);
-  printf("%ld ", buffer.st_ctime);
-  printf("%-6lld ", buffer.st_size);
-  printf("%-4lld ", buffer.st_blocks);
-  printf("%d\n", buffer.st_blksize);
+  // printf("%-50s: ", name);
+  // printf("%-6lld ", buffer.st_size);
+  // printf("%d ", buffer.st_dev);
+  // printf("%llu ", buffer.st_ino);
+  // printf("%d ", buffer.st_mode);
+  // printf("%d ", buffer.st_nlink);
+  // printf("%d ", buffer.st_uid);
+  // printf("%d ", buffer.st_gid);
+  // printf("%d ", buffer.st_rdev);
+  // printf("%ld ", buffer.st_atime);
+  // printf("%ld ", buffer.st_mtime);
+  // printf("%ld ", buffer.st_ctime);
+  // printf("%-4lld ", buffer.st_blocks);
+  // printf("%d\n", buffer.st_blksize);
+
+  char time_str[32];
+  strftime(time_str, sizeof(time_str), "%d %b %H:%M", localtime(&buffer.st_atime));
+
+  print_file_size(buffer.st_size);
+  printf("%s\n", name);
 }
 
 void dir_walk(char *dir_name, void (*func)(char *))
@@ -106,4 +113,20 @@ void dir_walk(char *dir_name, void (*func)(char *))
   }
 
   closedir(dir);
+}
+
+void print_file_size(size_t size)
+{
+  static const char *SIZES[] = {"B", "K", "M", "G"};
+  size_t div = 0;
+  size_t rem = 0;
+
+  while (size >= 1024 && div < (sizeof SIZES / sizeof *SIZES))
+  {
+    rem = (size % 1024);
+    div++;
+    size /= 1024;
+  }
+
+  printf("%6.1f%s ", (float)size + (float)rem / 1024.0, SIZES[div]);
 }
