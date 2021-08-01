acf682465ab480e1baeb853a3774a46daf8bab78
Daniel Costrasel <ohkimur@gmail.com>
2021-08-01 21:52:10 +0300

    chore: added malloc prototype and todo

--- chapter_8/exercise_8_06/calloc.c
+++ chapter_8/exercise_8_06/calloc.c
@@ -2,7 +2,14 @@
 #include <string.h>
 #include <fcntl.h>
 
+void *malloc(unsigned int nr_of_bytes);
+
 int main(int argc, char *argv[])
 {
   return 0;
+}
+
+void *malloc(unsigned int nr_of_bytes)
+{
+  // TOOD: Implement the memory allocation here.
 }
