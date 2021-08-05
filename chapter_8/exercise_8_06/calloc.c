ac6673a10813e1f23db78f9d7e7d9fab7aa00ad8
Daniel Costrasel <ohkimur@gmail.com>
2021-08-05 15:55:01 +0300

    chore: implemented malloc

--- chapter_8/exercise_8_06/calloc.c
+++ chapter_8/exercise_8_06/calloc.c
@@ -8,12 +8,16 @@ union header
   {
     union header *free_block_p;
     size_t size;
-  };
+  } s;
   Aling x; // Force alignment of blocks.
 };
 
 typedef union header Header;
 
+static Header base;
+static Header *free_p = NULL;
+
+Header *morecore(size_t nr_of_units);
 void *c_malloc(size_t nr_of_bytes);
 
 int main(int argc, char *argv[])
@@ -23,6 +27,43 @@ int main(int argc, char *argv[])
 
 void *c_malloc(size_t nr_of_bytes)
 {
-  // TOOD: Implement the memory allocation here.
+  Header *p;
+  Header *prev_p;
+  size_t nr_of_units = (nr_of_bytes + sizeof(Header) - 1) / sizeof(Header);
+
+  if ((prev_p = free_p) == NULL)
+  {
+    base.s.free_block_p = free_p = prev_p = &base;
+    base.s.size = 0;
+  }
+
+  for (p = prev_p->s.free_block_p;; prev_p = p, p = p->s.free_block_p)
+  {
+    if (p->s.size >= nr_of_units)
+    {
+      if (p->s.size == nr_of_units)
+      {
+        prev_p->s.free_block_p = p->s.free_block_p;
+      }
+      else
+      {
+        p->s.size -= nr_of_units;
+        p += p->s.size;
+        p->s.size = nr_of_units;
+      }
+
+      free_p = prev_p;
+      return (void *)(p + 1);
+    }
+
+    if (p == free_p)
+    {
+      if ((p = morecore(nr_of_units)) == NULL)
+      {
+        return NULL;
+      }
+    }
+  }
+
   return NULL;
 }
