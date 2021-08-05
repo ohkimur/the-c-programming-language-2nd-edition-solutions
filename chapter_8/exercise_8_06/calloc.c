7b1ac2ba035d80446160c9e158c677f00f414a6b
Daniel Costrasel <ohkimur@gmail.com>
2021-08-05 13:05:47 +0300

    chore: defined header block structure

--- chapter_8/exercise_8_06/calloc.c
+++ chapter_8/exercise_8_06/calloc.c
@@ -1,15 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
+#include <stdlib.h>
 
-void *malloc(unsigned int nr_of_bytes);
+typedef long Aling;
+
+union header
+{
+  struct
+  {
+    union header *free_block_p;
+    size_t size;
+  };
+  Aling x; // Force alignment of blocks.
+};
+
+typedef union header Header;
+
+void *c_malloc(size_t nr_of_bytes);
 
 int main(int argc, char *argv[])
 {
-  return 0;
+  return EXIT_SUCCESS;
 }
 
-void *malloc(unsigned int nr_of_bytes)
+void *c_malloc(size_t nr_of_bytes)
 {
   // TOOD: Implement the memory allocation here.
+  return NULL;
 }
