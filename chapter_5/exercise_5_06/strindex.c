660c7156cc849567d4554ab2205c3b97775d0d40
metalbuild-mnbv <83763751+metalbuild-mnbv@users.noreply.github.com>
2022-04-07 16:18:06 +0800

    Update strindex.c
    strindex of exercise_5_06 always return -1, if the char t[] is modified to "this"

--- chapter_5/exercise_5_06/strindex.c
+++ chapter_5/exercise_5_06/strindex.c
@@ -5,7 +5,7 @@ int strindex(char *s, char *t);
 int main(void)
 {
   char s[] = "this is first string";
-  char t[] = "first";
+  char t[] = "this";                  /* the original is "first" */ 
 
   printf("%d", strindex(s, t));
 
@@ -18,10 +18,9 @@ int strindex(char *s, char *t)
   char *second;
   int pos = 0;
 
-  while (*s++ != '\0')
-  {
-    ++pos;
-
+  while (*s != '\0')                  /* the original is "while (*s++ != '\0')", 
+                                      it skips the first element in the statement of "if (*s == *t)" */
+  {                                   /* ++pos is removed, it also skips the first position */
     if (*s == *t)
     {
       first = s;
@@ -33,6 +32,8 @@ int strindex(char *s, char *t)
           return pos;
       }
     }
+    pos++;                            /* add 1 if the condition above not ture */
+    s++;                              /* we can move to next character in next loop */
   }
 
   return -1;
