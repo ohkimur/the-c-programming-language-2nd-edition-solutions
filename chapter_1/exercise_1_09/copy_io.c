49354900ff4cb93129a6b92dfca66013916fcd5b
4rturkania <111567962+4rturkania@users.noreply.github.com>
2023-06-15 14:32:02 +0200

    Update copy_io.c
    changed chars to ints to avoid "comparison between pointer and integer gcc" warning

--- chapter_1/exercise_1_09/copy_io.c
+++ chapter_1/exercise_1_09/copy_io.c
@@ -2,8 +2,15 @@
 
 int main(void)
 {
-  char c;
-  char last_c = '\0';
+  int c;
+  int last_c = '\0';
+  /*
+    If I use chars here instead of ints I get a "comparison
+    between pointer and integer gcc" warning.
+    getchar returns int not char.
+    Char type is de facto a 1-byte integer type.
+    Thank you for you solutions, they're helping me learn.
+  */
   while ((c = getchar()) != EOF)
   {
     if (c != ' ' || last_c != ' ')
