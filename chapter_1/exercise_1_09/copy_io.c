85345859681855c5a890e7cb293f434b1f856073
4rturkania <111567962+4rturkania@users.noreply.github.com>
2023-07-10 13:26:31 +0200

    removed comment

--- chapter_1/exercise_1_09/copy_io.c
+++ chapter_1/exercise_1_09/copy_io.c
@@ -4,13 +4,7 @@ int main(void)
 {
   int c;
   int last_c = '\0';
-  /*
-    If I use chars here instead of ints I get a "comparison
-    between pointer and integer gcc" warning.
-    getchar returns int not char.
-    Char type is de facto a 1-byte integer type.
-    Thank you for you solutions, they're helping me learn.
-  */
+
   while ((c = getchar()) != EOF)
   {
     if (c != ' ' || last_c != ' ')
