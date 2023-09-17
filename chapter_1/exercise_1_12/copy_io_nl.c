b2c17bcd8ca0980bd97db4701e73f53914f1b746
Daniel Torres <daniel.torres@chainalysis.com>
2023-09-17 23:35:51 +0100

    fix formatting

--- chapter_1/exercise_1_12/copy_io_nl.c
+++ chapter_1/exercise_1_12/copy_io_nl.c
@@ -1,18 +1,18 @@
-#include <stdio.h>                                                               
-                                                                                 
-int main()                                                                       
-{                                                                                
-        char c;                                                                  
-        char last_char = -1;                                                     
-        while ((c = getchar()) != EOF) {                                         
-                if(c==' ' || c=='\t' ||  c=='\n'){                               
-                        if(last_char!=' ' &&  last_char!='\t' && last_char!='\n'){
-                                putchar('\n');                                   
-                        }                                                        
-                }                                                                
-                else {                                                           
-                        putchar(c);                                              
-                }       
-                last_char=c;
-        }                                                                        
-}        
+#include <stdio.h>
+
+int main()
+{
+  char c;
+  char last_char = -1;
+  while ((c = getchar()) != EOF) {
+    if(c==' ' || c=='\t' ||  c=='\n'){
+      if(last_char!=' ' &&  last_char!='\t' && last_char!='\n'){
+        putchar('\n');
+      }
+    }
+    else {
+      putchar(c);
+    }
+    last_char=c;
+  }
+}
