a8433b9e440dddcc64b5039ed987db3485146200
Daniel Torres <daniel.torres@chainalysis.com>
2023-09-18 13:30:26 +0100

    simplify logic

--- chapter_1/exercise_1_12/copy_io_nl.c
+++ chapter_1/exercise_1_12/copy_io_nl.c
@@ -6,16 +6,18 @@ int main()
   char last_char = -1;
 
   while ((c = getchar()) != EOF) {
-    if(c==' ' || c=='\t' ||  c=='\n'){
-      if(last_char!=' ' &&  last_char!='\t' && last_char!='\n'){
+    if((c==' ' ||  c=='\t' ||  c=='\n')) {
+      if(c != last_char) {
         putchar('\n');
       }
     }
     else {
       putchar(c);
     }
+
     last_char=c;
   }
 
   return 0;
 }
+
