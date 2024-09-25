c03cc23431a5d3cf454085f9a0119edc50a46406
Jerry Qi <jerryq0101@gmail.com>
2024-09-24 21:00:29 -0400

    fix

--- chapter_1/exercise_1_17/line_80.c
+++ chapter_1/exercise_1_17/line_80.c
@@ -10,19 +10,22 @@ int main(void) {
     char continuous[THRESHOLD];
 
     while ((len = get_line(first, THRESHOLD)) > 0) {
-        
         if (len == THRESHOLD-1) {
+            // length contains \n (so +1 to actual count)
             printf("%s", first);
             nextLen = THRESHOLD-1;
-            while (nextLen == THRESHOLD-1) {
-                nextLen = get_line(continuous, THRESHOLD);
-                printf("%s", continuous);
+            
+            // check if the string terminated at exactly the 81st place by a newline.
+            // If so, no need to look for new segments of same line input.
+            if (first[81] != '\n') {
+                while (nextLen == THRESHOLD-1) {
+                    nextLen = get_line(continuous, THRESHOLD);
+                    printf("%s", continuous);
+                }
             }
         }
         len = 0;
     }
-
-    return 0;
 }
 
 int get_line(char line[], int max_line_len)
