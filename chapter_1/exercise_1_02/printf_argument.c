a01ba79b83c1e3d5a27aaf8501747422f8cb43a5
ayush <152896667+kkaname@users.noreply.github.com>
2025-12-15 11:18:42 +0530

    Fix printf argument to avoid warning
    Removed warning comment about unknown escape sequence.

--- chapter_1/exercise_1_02/printf_argument.c
+++ chapter_1/exercise_1_02/printf_argument.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("hello, world\c");     /*prints warning: unknown escape sequence '\c' */
+    printf("hello, world\c");    
     return 0;
 }
+
+// prints warning: unknown escape sequence '\c'
